6fe74c4a9d13d7ebb2aaa4234060ebf1c2f41aec
Alberto Chirinos <developernet@outlook.com>
2020-08-07 20:13:14 -0500

    Ajustes en funcionalidad de view Create de Seccion

---
+++ ProEducationalM/ProEducationalM/Scripts/Generales/Caracteres.js
@@ -0,0 +1,31 @@
+﻿// Función para reemplazar caracteres especiales
+function TratamientoCaracteres(cadena) {
+
+    // &#
+    let termino = "&#";
+
+    let posicion = cadena.indexOf(termino);
+
+    if (posicion !== -1) {
+
+        cadena = cadena.replace(termino, "");
+
+    }
+
+    // <> como primer y último caracter
+    termino = "<>";
+
+    let primerCaracter = cadena.substring(0, 1);
+
+    let ultimoCaracter = cadena.substr(-1);
+
+    let combinacion = primerCaracter + ultimoCaracter;
+
+    if (combinacion === termino) {
+
+        cadena = cadena.substring(1, cadena.length - 1);
+
+    }
+
+    return cadena;
+}
